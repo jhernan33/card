5becccd05718e4e4e3177e00b3a37ce04cceb701
Hernan <jhernan33@gmail.com>
2022-08-26 10:11:10 -0400

    Update Model Token and Serializer, Urls, View Business Card and Template CompanyList

--- business/models/token.py
+++ business/models/token.py
@@ -1,9 +1,13 @@
 from django.db import models
 
 class Token(models.Model):
-    userId = models.IntegerField(null=False, blank=False)
+    userId = models.IntegerField(null=True, blank=True)
     token = models.CharField(max_length=255,null=True, blank=True)
     status = models.BooleanField(default=True,null=True, blank=True)
+    companyId = models.IntegerField(null=True, blank=True)
+    created  =models.DateTimeField(auto_now=False, null=True,auto_now_add=False,blank=True)
+    updated  =models.DateTimeField(auto_now=False, null=True,auto_now_add=False,blank=True)
+    deleted  =models.DateTimeField(auto_now=False, null=True,auto_now_add=False,blank=True)
 
     class Meta:
         db_table = 'tbl_token'
--- business/urls.py
+++ business/urls.py
@@ -8,12 +8,21 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     # Company Card
-    path('card/',                  views.UsersCardListView.as_view(),           name='companyCard'),
-    path('card/<int:id>/',         views.UsersCardRetrieveView.as_view(),       name='companyCardShow'),
-    path('card/create/',           views.UserCardCreateView.as_view(),          name='companyCardSCreate'),
-    path('user/',                  views.UsersListView.as_view(),               name="userList"),
-    path('card/list/',             views.CardUserListView.as_view(),            name="ListCard"),
-    path('business/card/',         views.BusinessCardListView.as_view(),        name="BusinessCard"),
+    # path('card/',                     views.UsersCardListView.as_view(),           name='companyCard'),
+    # path('card/<int:id>/',            views.UsersCardRetrieveView.as_view(),       name='companyCardShow'),
+    # path('card/create/',              views.UserCardCreateView.as_view(),          name='companyCardSCreate'),
+    # path('user/',                     views.UsersListView.as_view(),               name="userList"),
+
+    # ********* Form Register Token Company ********* 
+    path('addTokenCompany/',            views.TokenCompanyListView.as_view(),            name="ListCard"),
+    # *********************************************** 
+
+    # Register Token Company        
+    path('token/company/',              views.TokenCreateView.as_view(),               name="TokenCompany"),
+    # Form Company List Token       
+    path('company/',                    views.CardUserListView.as_view(),            name="ListCard"),
+    # Search Target Company     
+    path('businessCard/',               views.BusinessCardListView.as_view(),        name="BusinessCard"),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
  
  
